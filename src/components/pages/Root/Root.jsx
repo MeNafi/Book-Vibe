1e84ba9803ab06c40741f4da29c02dff1b964a91
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-18 20:45:23 +0600

    Routes setup done

---
+++ src/components/pages/Root/Root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../Header/Navbar';
+import Footer from '../../Footer/Footer';
+
+const Root = () => {
+    return (
+        <div>
+          <Navbar></Navbar>
+          <Outlet></Outlet>
+          <Footer></Footer> 
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import {RouterProvider} from "react-router";
+import { router } from './Routes/Routes.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />,
+     <RouterProvider router={router} />
   </StrictMode>,
 )
