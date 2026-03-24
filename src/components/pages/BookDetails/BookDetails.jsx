840be7d2c4d80cee1ab0625f7b4b1a46d2915989
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-24 20:58:12 +0600

    All book pages button and view part half done

--- src/components/pages/BookDetails/BookDetails.jsx
+++ src/components/pages/BookDetails/BookDetails.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const BookDetails = () => {
-    
-    const id = useParams();
-    console.log(id);
+  const { id } = useParams();
+  const bookId = parseInt(id);
+  const data = useLoaderData();
+  const singleBook = data?.find((book) => book.bookId === bookId);
+  const { bookName, image } = singleBook;
 
-    return (
-        <div>
-            <h1>Hey i am bookDetails</h1>
-        </div>
-    );
+  return (
+    <div className="w-2/3 mx-auto">
+      <img src={image} alt="" />
+      <h5>{bookName}</h5>
+      <button className="btn btn-accent m-2">Read</button>
+      <button className="btn btn-info m-2">Wishlist</button>
+    </div>
+  );
 };
 
 export default BookDetails;
