95f524f7a330802bb5802d645efec92de39e1f7c
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-24 23:36:22 +0600

    Banner part half done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import bookimage from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
-      <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src={bookimage}
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
+        /* container and py-12/my-8 create the gaps from navbar and bottom content */
+        <div className="container mx-auto my-8 px-4">
+            <div className="hero bg-base-200 rounded-3xl min-h-[500px] px-8 lg:px-24">
+                <div className="hero-content flex-col lg:flex-row-reverse justify-between w-full">
+                    {/* Famous JS Book Image  */}
+                    <img
+                        src="https://www.designveloper.com/wp-content/uploads/2021/01/Classic-Books-about-JavaScript-The-Definitive-Guide-1024x1024.jpg"
+                        className="max-w-xs lg:max-w-sm rounded-lg shadow-2xl"
+                        alt="Eloquent JavaScript"
+                    />
+                    
+                    {/* Left side text container */}
+                    <div className="max-w-md lg:max-w-lg">
+                        <h1 className="text-4xl lg:text-6xl font-bold leading-tight text-[#131313]">
+                            Books to freshen up <br /> your bookshelf
+                        </h1>
+                        
+                        <div className="mt-8 lg:mt-12">
+                            {/* Green button */}
+                            <button className="btn bg-[#23BE0A] hover:bg-[#1fa308] text-white border-none px-7 py-4 h-auto font-bold text-xl normal-case rounded-lg">
+                                View The List
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
