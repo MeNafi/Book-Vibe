3f5618523c86873af300a910f989a4fafae4ed44
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-24 23:00:37 +0600

    set the local storage function

--- src/components/pages/BookDetails/BookDetails.jsx
+++ src/components/pages/BookDetails/BookDetails.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { addToStoredDB } from "../../../utility/addToDB";
 
 const BookDetails = () => {
   const { id } = useParams();
   const bookId = parseInt(id);
   const data = useLoaderData();
-  const singleBook = data?.find((book) => book.bookId === bookId);
+  const singleBook = data.find((book) => book.bookId === bookId);
   const { bookName, image } = singleBook;
+
+ const handleMarkAsRead = id => {
+  // Store with id
+  // where to store
+  // array of collection
+  // if book already exist the show a alert
+  // if book not exist then push in the collection or array
+  addToStoredDB(id)
+ }
 
   return (
     <div className="w-2/3 mx-auto">
       <img src={image} alt="" />
       <h5>{bookName}</h5>
-      <button className="btn btn-accent m-2">Read</button>
-      <button className="btn btn-info m-2">Wishlist</button>
+      <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as Read</button>
+      <button className="btn btn-info m-2">Add To Wishlist</button>
     </div>
   );
 };
